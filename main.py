41eb9c86c44fcb2011b08f834c75d7fa91682e8f
Fisher Mac <miguel@regala.im>
2019-08-10 18:16:56 -0700

    added radio buttons. fixed proper sorting and showing using a second array list

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*- 
 from burp import IBurpExtender
 from burp import ITab
 from burp import IHttpListener
 from burp import IMessageEditorController
+from burp import IContextMenuFactory
 from java.awt import Component;
 from java.io import PrintWriter;
 from java.util import ArrayList;
@@ -15,10 +17,32 @@ from javax.swing.table import AbstractTableModel;
 from threading import Lock
 
 ###
+from javax.swing import JMenuItem
+from javax.swing import JPopupMenu
+from java.awt.event import ActionListener
+from java.awt import BorderLayout
+from java.awt import GridLayout
+from javax.swing import ButtonGroup
+from javax.swing import JRadioButton
+from javax.swing import JLabel
+from javax.swing import JButton
+from javax.swing import JComboBox
+from javax.swing import JCheckBox
+from javax.swing import JPanel
+from javax.swing import SortOrder
+from java.lang import Runnable
+from javax.swing import RowFilter
+from java.awt.event import ItemListener
+from javax.swing.table import TableRowSorter
 from urlparse import *
 
+BAD_EXTENSIONS = ['.gif', '.png', '.js', '.woff', '.woff2', '.jpeg', '.jpg', '.css', '.ico']
 
-class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController, AbstractTableModel):
+SHOW_ALL_BUTTON_LABEL = "Show All"
+SHOW_NEW_BUTTON_LABEL = "Show New Only"
+SHOW_TEST_BUTTON_LABEL = "Show Tested Only"
+
+class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController, AbstractTableModel, IContextMenuFactory):
     
     #
     # implement IBurpExtender
@@ -33,17 +57,59 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
         
         # set our extension name
         callbacks.setExtensionName("Burp Scope Monitor")
+
+        #self._showAllFlag = True
+        #self._showNewOnly = False
+        # - quero ver só os novos
+        # - quero ver todos
+        # 
         
         # create the log and a lock on which to synchronize when adding log entries
         self._log = ArrayList()
+        self._fullLog = ArrayList()
         self._lock = Lock()
         
         # main split pane
+        self._parentPane = JSplitPane(JSplitPane.HORIZONTAL_SPLIT)
+
         self._splitpane = JSplitPane(JSplitPane.VERTICAL_SPLIT)
+
+        ##### config pane
+        self._config = JTabbedPane()
+
+        config = JPanel()
+        config.setLayout(GridLayout(1,3))
+
+        self.showAllButton = JRadioButton(SHOW_ALL_BUTTON_LABEL, True)
+        self.showNewButton = JRadioButton(SHOW_NEW_BUTTON_LABEL, False)
+        self.showTestedButton = JRadioButton(SHOW_TEST_BUTTON_LABEL, False)
+
+        self.showAllButton.addActionListener(self.handleRadioConfig)
+        self.showNewButton.addActionListener(self.handleRadioConfig)
+        self.showTestedButton.addActionListener(self.handleRadioConfig) 
+
+
+        bGroup = ButtonGroup()
+
+        bGroup.add(self.showAllButton)
+        bGroup.add(self.showNewButton)
+        bGroup.add(self.showTestedButton)
+
+        config.add(self.showAllButton)
+        config.add(self.showNewButton)
+        config.add(self.showTestedButton)
+
+        self._config.addTab("Config", config)
+
+        ##### end config pane
+
+
+        self._parentPane.setLeftComponent(self._splitpane)
+        self._parentPane.setRightComponent(self._config)
         
         # table of log entries
-        logTable = Table(self)
-        scrollPane = JScrollPane(logTable)
+        self.logTable = Table(self)
+        scrollPane = JScrollPane(self.logTable)
         self._splitpane.setLeftComponent(scrollPane)
 
         # tabs with request/response viewers
@@ -53,12 +119,33 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
         tabs.addTab("Request", self._requestViewer.getComponent())
         tabs.addTab("Response", self._responseViewer.getComponent())
         self._splitpane.setRightComponent(tabs)
+
+        self._tableRowSorterAutoProxyAutoAction = CustomTableRowSorter(self.logTable.getModel())
+        self.logTable.setRowSorter(self._tableRowSorterAutoProxyAutoAction)
+        #self.tableSorter = TableRowSorter(self)
+        #filter = tableFilter(self)
+        #self.tableSorter.setRowFilter(filter)
+        #logTable.setRowSorter(self.tableSorter)
         
+
+        copyURLitem = JMenuItem("Copy URL")
+        copyURLitem.addActionListener(copySelectedURL(self))
+
+        #sendRequestMenu = JMenuItem("Send Original Request to Repeater")
+        #sendRequestMenu.addActionListener(sendRequestRepeater(self, self._callbacks, True))
+        self.menu = JPopupMenu("Popup")
+        self.menu.add(copyURLitem)
+
         # customize our UI components
+        callbacks.customizeUiComponent(self._parentPane)
         callbacks.customizeUiComponent(self._splitpane)
-        callbacks.customizeUiComponent(logTable)
+        callbacks.customizeUiComponent(self._config)
+        callbacks.customizeUiComponent(config)
+        callbacks.customizeUiComponent(self.logTable)
         callbacks.customizeUiComponent(scrollPane)
         callbacks.customizeUiComponent(tabs)
+
+        callbacks.registerContextMenuFactory(self)
         
         # add the custom tab to Burp's UI
         callbacks.addSuiteTab(self)
@@ -69,6 +156,22 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
         return
         
     ##### CUSTOM CODE #####
+
+    def createMenuItems(self, invocation):
+        responses = invocation.getSelectedMessages()
+        print 'trying to create menuItems..'
+        if responses > 0:
+            ret = LinkedList()
+            requestMenuItem = JMenuItem("XXX Send request to Autorize")
+            cookieMenuItem = JMenuItem("XXX Send cookie to Autorize")
+
+            for response in responses:
+                requestMenuItem.addActionListener(handleMenuItems(self,response, "request"))
+                cookieMenuItem.addActionListener(handleMenuItems(self, response, "cookie"))   
+            ret.add(requestMenuItem)
+            ret.add(cookieMenuItem)
+            return ret
+
 
     def getEndpoint(self, requestResponse):
         url_ = str(self._helpers.analyzeRequest(requestResponse).getUrl())
@@ -85,6 +188,42 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
 
     ##### CUSTOM CODE #####
 
+    def handleRadioConfig(self, event):
+        #print ' radio button clicked '
+        #print event.getActionCommand()
+        self._lock.acquire()
+
+        if event.getActionCommand() == SHOW_ALL_BUTTON_LABEL:
+            print "Showing all"
+            self._log = self._fullLog
+        elif event.getActionCommand() == SHOW_NEW_BUTTON_LABEL:
+            print "Showing new scope only"
+            tmpLog = ArrayList()
+            for item in self._fullLog:
+                if not(item._analyzed):
+                    tmpLog.add(item)
+            self._log = tmpLog
+        elif event.getActionCommand() == SHOW_TEST_BUTTON_LABEL:
+            print "Showing tested scope only"
+            tmpLog = ArrayList()
+            for item in self._fullLog:
+                if item._analyzed:
+                    tmpLog.add(item)
+            self._log = tmpLog
+        else:
+            print "unrecognized radio label"
+
+        
+        self.fireTableDataChanged()
+        self._tableRowSorterAutoProxyAutoAction.toggleSortOrder(1)
+        #self.toggleSortOrder(2)
+        
+        #self.logTable.toggleSortOrder(2)
+
+        # refresh table?
+
+        self._lock.release()
+
 
     #
     # implement ITab
@@ -94,7 +233,7 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
         return "Scope Monitor"
     
     def getUiComponent(self):
-        return self._splitpane
+        return self._parentPane
         
     #
     # implement IHttpListener
@@ -104,12 +243,21 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
         # only process requests
         if messageIsRequest:
             return
+
+        #if (self._callbacks.getToolName(toolFlag) != "Proxy") or (self._callbacks.getToolName(toolFlag) != "Repeater"):
+        #    return
+
+        url = self.getEndpoint(messageInfo)
+
+        for extension in BAD_EXTENSIONS:
+            if url.endswith(extension):
+                return
         
         # create a new log entry with the message details
         self._lock.acquire()
         row = self._log.size()
 
-        url = self.getEndpoint(messageInfo)
+
         for item in self._log:
             if url == item._url:
                 print 'duplicate url, skipping.. '
@@ -117,7 +265,14 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
                 return
 
         # reached here, must be new entry
-        self._log.add(LogEntry(toolFlag, self._callbacks.saveBuffersToTempFiles(messageInfo), url))
+        analyzed = False
+        if self._callbacks.getToolName(toolFlag) == "Repeater":
+            analyzed = True
+
+        entry = LogEntry(toolFlag, self._callbacks.saveBuffersToTempFiles(messageInfo), url, analyzed)
+        print "toolFlag: " + str(toolFlag)
+        self._log.add(entry)
+        self._fullLog.add(entry)
         self.fireTableRowsInserted(row, row)
         self._lock.release()
 
@@ -143,11 +298,27 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
 
     def getValueAt(self, rowIndex, columnIndex):
         logEntry = self._log.get(rowIndex)
+
+        #self.showAllButton.isSelected()
+        return self.returnEntry(rowIndex, columnIndex, logEntry)
+
+        if self.showNewButton.isSelected() and not(logEntry._analyzed):  
+            return self.returnEntry(rowIndex, columnIndex, logEntry)
+        elif self.showTestedButton.isSelected() and logEntry._analyzed:
+            return self.returnEntry(rowIndex, columnIndex, logEntry)
+        elif self.showAllButton.isSelected():
+            return self.returnEntry(rowIndex, columnIndex, logEntry)
+
+
+    def returnEntry(self, rowIndex, columnIndex, entry):
+        logEntry = self._log.get(rowIndex)
         if columnIndex == 0:
+            return logEntry._analyzed
             return self._callbacks.getToolName(logEntry._tool)
         if columnIndex == 1:
             return logEntry._url
         return ""
+
 
     #
     # implement IMessageEditorController
@@ -174,6 +345,8 @@ class Table(JTable):
     
     def changeSelection(self, row, col, toggle, extend):
     
+        # shows "entries" matching
+
         # show the log entry for the selected row
         logEntry = self._extender._log.get(row)
         self._extender._requestViewer.setMessage(logEntry._requestResponse.getRequest(), True)
@@ -187,9 +360,68 @@ class Table(JTable):
 #
 
 class LogEntry:
-    def __init__(self, tool, requestResponse, url):
+    def __init__(self, tool, requestResponse, url, analyzed):
         self._tool = tool
         self._requestResponse = requestResponse
         self._url = url
         self._displayed = False
-        self._analyzed  = False
+        self._analyzed  = analyzed
+
+class CustomTableRowSorter(TableRowSorter):
+
+    # override toggleSortOrder method
+    def toggleSortOrder(self, column):
+
+        # check if valid column 
+        if column >= 0:
+
+            # get the sort keys
+            keys = self.getSortKeys()
+
+            # check if the sort keys are not empty
+            if keys.isEmpty() == False:
+
+                # get the sort key
+                sortKey = keys.get(0)
+
+                # check if the column clicked is sorted in descending order
+                if sortKey.getColumn() == column and sortKey.getSortOrder() == SortOrder.DESCENDING:
+
+                    # clear sorting
+                    self.setSortKeys(None)
+
+                    # do not continue
+                    return
+
+        # try to toggle default toggleSortOrder
+        try:
+            # toggle default toggleSortOrder
+            TableRowSorter.toggleSortOrder(self, column)
+
+        # catch if table is being sorted by processProxyMessage and user
+        except:
+            pass
+
+class copySelectedURL(ActionListener):
+    def __init__(self, extender):
+        self._extender = extender
+
+    def actionPerformed(self, e):
+        stringSelection = StringSelection(str(self._extender._helpers.analyzeRequest(self._extender._currentlyDisplayedItem._requestResponse).getUrl()))
+        clpbrd = Toolkit.getDefaultToolkit().getSystemClipboard()
+        clpbrd.setContents(stringSelection, None)
+
+class handleMenuItems(ActionListener):
+    def __init__(self, extender, messageInfo, menuName):
+        self._extender = extender
+        self._menuName = menuName
+        self._messageInfo = messageInfo
+
+    def actionPerformed(self, e):
+        if self._menuName == "request":
+            print "do request"
+            #start_new_thread(self._extender.sendRequestToAutorizeWork,(self._messageInfo,))
+
+        if self._menuName == "cookie":
+            print "do ciikie"
+            #self._extender.replaceString.setText(self._extender.getCookieFromMessage(self._messageInfo))
